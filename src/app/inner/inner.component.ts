cfdcd0e8389c5ecea3c36a2f3981e945e8cf1f7c
Ambuj Khanna <Ambuj Khanna>
2017-07-22 13:51:35 +0530

    ngfor and ngIF practice

--- src/app/inner/inner.component.ts
+++ src/app/inner/inner.component.ts
@@ -3,13 +3,28 @@ import {Component} from '@angular/core';
 
 @Component({
     selector : 'app-inner',
-    templateUrl : './inner.component.html'
+    templateUrl : './inner.component.html',
+    styles : ['div, span{color:#666; font-size:14px;padding:5px 0}']
 })
 
-export class innerComponent{
+export class innerComponent {
     title = "innerComponent text";
     titleText = "This is custom Text";
 
+    showFlag = 1;
+    view = "one"
+    colors = ['red', 'green', 'blue', 'pink', 'yellow'];
 
+    items=[
+        {
+            name :'Mark',age:'25',gender:'M'
+        },
+         {
+            name :'Steve',age:'28',gender:'M'
+        },
+        {
+            name :'Robbin',age:'32',gender:'M'
+        }
+    ]
 
 }
