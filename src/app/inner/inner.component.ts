8c7fa3fb29aa7fc6a873618a9fd7b143773e08bf
ambujkhanna <ambuj.webmasters@gmail.com>
2017-07-24 10:05:48 +0530

    home changes

--- src/app/inner/inner.component.ts
+++ src/app/inner/inner.component.ts
@@ -10,6 +10,35 @@ export class innerComponent{
     title = "innerComponent text";
     titleText = "This is custom Text";
 
+    allowclick = false;
+    currentStatus;
+    
+    repeatItems = ['item 1','item2', 'item 3'];
 
+
+    constructor(){
+      setTimeout(() => {
+        this.allowclick = true;
+      },2000);
+  
+      this.currentStatus = Math.random() > 0.5 ? 'Online' : 'Offline'; 
+  
+    }
+
+    ShowData='';
+    EnteredData='';
+
+    onClicked (){
+        this.ShowData="'"+this.EnteredData + "' is textbox Data ";
+    }
+    
+    getColor(){
+        return this.currentStatus ==='online' ? 'green' :'red';
+    }
+    eachItem = '';
+    addItems(){
+        this.repeatItems.push(this.eachItem);
+    }
+    
 
 }
